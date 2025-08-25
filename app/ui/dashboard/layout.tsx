aceb08341a9e381a14dacf4387089bd3d36891f3
Trần Thành Long <ttlong21022000@gmail.com>
2025-08-25 15:28:11 +0700

    fix errors during construction

--- app/ui/dashboard/layout.tsx
+++ app/ui/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 // export const experimental_ppr = true;
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
