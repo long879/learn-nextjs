224209497f8e835c327dddfbff284668330d16e7
Trần Thành Long <ttlong21022000@gmail.com>
2025-08-25 15:31:25 +0700

    re-add condition after fixing error

--- app/ui/dashboard/layout.tsx
+++ app/ui/dashboard/layout.tsx
@@ -1,7 +1,6 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
-import React from "react";
 
-// export const experimental_ppr = true;
+export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
